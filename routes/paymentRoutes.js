30dd8685644ebdb5e25e65a0a884b2c5d1928166
Orie Kanu <kanuorie3@gmail.com>
2026-06-02 02:27:02 +0100

    fixed backend

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { verifyPayment } from "../controllers/paymentController.js";
-import protect from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/verify", protect, verifyPayment);
-
-export default router;
